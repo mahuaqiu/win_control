2ffc6ab6dbdc392051bc155869e7fa4f296a17a9
majy2026 <372840680@qq.com>
2026-05-10 14:00:41 +0800

    feat: add base exception class WinCtrlError
    使用 PyO3 的 create_exception 宏创建公共异常基类 WinCtrlError，
    继承自 PyException，作为 winctrl 包所有自定义异常的基类。

--- src/error.rs
+++ src/error.rs
@@ -1,26 +1,4 @@
-use pyo3::prelude::*;
+use pyo3::create_exception;
 use pyo3::exceptions::PyException;
 
-// 自定义错误类型
-#[derive(Debug)]
-pub enum WinctrlError {
-    DisplayError(String),
-    AudioError(String),
-    SystemError(String),
-}
-
-impl std::fmt::Display for WinctrlError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            WinctrlError::DisplayError(msg) => write!(f, "Display error: {}", msg),
-            WinctrlError::AudioError(msg) => write!(f, "Audio error: {}", msg),
-            WinctrlError::SystemError(msg) => write!(f, "System error: {}", msg),
-        }
-    }
-}
-
-impl From<WinctrlError> for PyErr {
-    fn from(err: WinctrlError) -> PyErr {
-        PyException::new_err(err.to_string())
-    }
-}
+create_exception!(winctrl, WinCtrlError, PyException);
--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,11 @@ mod error;
 mod display;
 mod audio;
 
+use error::WinCtrlError;
+
 #[pymodule]
 fn winctrl(_py: Python<'_>, m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add("WinCtrlError", _py.get_type_bound::<WinCtrlError>())?;
     m.add_submodule(&display::create_module(_py)?)?;
     m.add_submodule(&audio::create_module(_py)?)?;
     Ok(())
